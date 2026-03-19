1496484aa1076bf1a91469ed2c7b1a9ae7614914
sncthyz <andikacahyarahman123@gmail.com>
2026-03-19 20:35:22 +0700

    commit session-6

---
+++ src/models/user.model.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface IUser extends Document {
+    email:string;
+    password:string;
+    name:string;
+
+}
+
+const UserSchema: Schema = new Schema(
+    {
+        email:  {type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        name: { type: String, required: true },
+
+    }, 
+    { timestamps: true },
+);
+
+export default mongoose.model<IUser>("User", UserSchema);
